9bc42d079db8be61930a4b630039b5ab2395e1bd
austieoduor <austineoduor57@gmail.com>
2023-02-09 13:33:33 +0000

    base correction

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -17,9 +17,13 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """ Get an item by key
-        """
+        
         if key is None or key not in self.cache_data:
             return None
         else:
             for key in self.cache_data:
                 return self.cache_data[key]
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
